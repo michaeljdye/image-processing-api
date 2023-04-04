fdbe9c0d32969090bcf5198fc08cb5b2586b4619
Michael Dye <hello@michaeldye.dev>
2023-04-04 10:53:26 -0500

    feat: Add eslint and prettier configs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
 });
