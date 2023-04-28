5e3d14810de5ad6da17a0057a27b20a2a2dff476
Michael Dye <michael@Michaels-MBP.attlocal.net>
2023-04-28 11:36:46 -0500

    feat: Add image processing

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from 'express';
 import sharp from 'sharp';
+import { promises as fs } from 'fs';
 
 export const resizeImage = async (req: Request, res: Response) => {
-  const { width, height, filename } = req.query;
-
-  res.send(`width: ${width}, height: ${height}, filename: ${filename}`)
+  const { width = 200, height = 100, filename = 'mountain.jpg' } = req.query;
 
 
-  // sharp(req.params.image)
-  //   .resize(200, 300, {
-  //     kernel: sharp.kernel.nearest,
-  //     fit: 'contain',
-  //     position: 'right top',
-  //     background: { r: 255, g: 255, b: 255, alpha: 0.5 },
-  //   })
-  //   .toFile('output.png')
-  //   .then(() => {
-  //     // output.png is a 200 pixels wide and 300 pixels high image
-  //     // containing a nearest-neighbour scaled version
-  //     // contained within the north-east corner of a semi-transparent white canvas
-  //   });
+  const image = await fs.readFile(`src/images/full/${String(filename)}`);
+
+  sharp(image)
+    .resize(+height, +width, {
+      kernel: sharp.kernel.nearest,
+      fit: 'contain',
+      position: 'right top',
+      background: { r: 255, g: 255, b: 255, alpha: 0.5 },
+    })
+    .toFile(`src/images/thumb/${String(filename)}`)
+    .then(() => {
+      res.send('image resized')
+    });
 };
