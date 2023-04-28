a504e186a2d2480ca32181dde3dd1f7ab3710c54
Michael Dye <michael@Michaels-MBP.attlocal.net>
2023-04-28 11:55:02 -0500

    feat: Send back image file

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { Request, Response } from 'express';
 import sharp from 'sharp';
 import { promises as fs } from 'fs';
@@ -6,17 +7,15 @@ export const resizeImage = async (req: Request, res: Response) => {
   const { width = 200, height = 100, filename = 'mountain.jpg' } = req.query;
 
 
-  const image = await fs.readFile(`src/images/full/${String(filename)}`);
+  const image = await fs.readFile(`src/public/images/full/${String(filename)}`);
 
   sharp(image)
-    .resize(+height, +width, {
-      kernel: sharp.kernel.nearest,
-      fit: 'contain',
-      position: 'right top',
-      background: { r: 255, g: 255, b: 255, alpha: 0.5 },
-    })
-    .toFile(`src/images/thumb/${String(filename)}`)
+    .resize(+height, +width)
+    .toFile(`src/public/images/thumb/${String(filename)}`)
     .then(() => {
-      res.send('image resized')
+      const imagePath = path.resolve(`src/public/images/thumb/${String(filename)}`);
+      
+      res.setHeader('Content-Type', 'image/png');
+      res.sendFile(imagePath);
     });
 };
