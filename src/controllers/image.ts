55eebce517ba0f3b8ed61470fb7bed6fc1f25b0f
Michael Dye <hello@michaeldye.dev>
2023-05-02 15:44:56 -0500

    feat: Add image caching

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -4,18 +4,35 @@ import sharp from 'sharp';
 import { promises as fs } from 'fs';
 
 export const resizeImage = async (req: Request, res: Response) => {
-  const { width = 200, height = 100, filename = 'mountain.jpg' } = req.query;
+  const { width = 200, height = 100, filename = 'mountain' } = req.query;
 
+  try {
+    const thumbImage = await fs.readFile(`src/assets/images/thumb/${String(filename)}_thumb_${String(width)}_${String(height)}.jpg`);
 
-  const image = await fs.readFile(`src/public/images/full/${String(filename)}`);
-
-  sharp(image)
-    .resize(+height, +width)
-    .toFile(`src/public/images/thumb/${String(filename)}`)
-    .then(() => {
-      const imagePath = path.resolve(`src/public/images/thumb/${String(filename)}`);
-      
-      res.setHeader('Content-Type', 'image/png');
+    if (thumbImage) {
+      const imagePath = path.resolve(`src/assets/images/thumb/${String(filename)}_${String(width)}}_${String(height)}`);
       res.sendFile(imagePath);
-    });
+     return
+    }
+    
+  } catch (errorIfThumbImageNotFound) {
+    try {
+      const image = await fs.readFile(`src/assets/images/full/${String(filename)}.jpg`);
+
+      sharp(image)
+        .resize(+height, +width)
+        .toFile(`src/assets/images/thumb/${String(filename)}_thumb_${String(width)}_${String(height)}.jpg`)
+        .then(() => {
+          const imagePath = path.resolve(`src/assets/images/thumb/${String(filename)}_${String(width)}}_${String(height)}`);
+          
+          res.setHeader('Content-Type', 'image/png');
+          res.sendFile(imagePath);
+        });
+      
+    } catch (errorIfImageNotFound) {
+      res.status(404).send('Image not found');
+    }
+
+  }
+
 };
